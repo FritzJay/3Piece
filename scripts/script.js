e1c40a229e526bbd9111871d9416ea1fcc293268
Fritz Jay <Jonathan.jay10@gmail.com>
2017-04-23 19:11:17 -0700

    started playSound().

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,22 @@
+// Set consts
 const drums = document.getElementById('drums');
+const drumKeys = drums.querySelectorAll('button');
 const guitar = document.getElementById('guitar');
 const bass = document.getElementById('bass');
 
-const drumKeys = drums.querySelectorAll('button');
-drumKeys.forEach(key => key.addEventListener('click', playSound));
-
+//Create functions
 function playSound(e) {
-  console.log('playSound');
+  console.log(e);
+  // Get audio with corresponding data-key
+  const audio = document.querySelector(`audio[data-key="${e.srcElement.dataset.key}"]`);
+  console.log(audio);
+  // play audio
+  // start button transition
 }
 
 function removeTransition(e) {
 
 }
+
+// Add event listeners
+drumKeys.forEach(key => key.addEventListener('click', playSound));
