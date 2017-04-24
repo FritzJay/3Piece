f2dc0b796536cd5c7c3fedc71e05f255f3e357b1
Fritz Jay <Jonathan.jay10@gmail.com>
2017-04-23 23:23:49 -0700

    Added a check to playSound that cancles the sound from playing if the button activated doesn't contain .active

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,21 @@
 // Set consts
 const drummer = document.getElementById('drummer');
-const drumKeys = drummer.querySelectorAll('button');
 const guitar = document.getElementById('guitar');
 const bass = document.getElementById('bass');
 
 // Flags
 let isRecording = false;
-let isDrums = false;
-let isGuitar = false;
-let isBass = false;
 
 function playSound(e) {
+  console.log('playSound');
   // Get button. Adjusted the querySelector depending on event type
   let button = (e.type != 'keypress') ? e.srcElement : document.querySelector(`button[data-key="${e.keyCode}"]`);
+
+  //Return if button is null or isn't active
+  if (!button) {
+    return;
+    if (!button.classList.contains('active')) return;
+  }
   // Get audio with corresponding data-key
   let audio = document.querySelector(`audio[data-key="${button.dataset.key}"]`)
   // FROM THE TOP (reset audio.currentTime to 0. This allows us to play sounds quicker)
@@ -35,36 +38,33 @@ function removePlaying(e) {
 }
 
 function toggleActive(e) {
-  console.log('activating');
   // get div of character soon to be set to active
   const div = e.srcElement;
-  console.log(div);
   // if div contains .keys return
   if (div.classList.contains('keys')) {
     return;
   }
-
   // get keys of character soon to be set to active
   const keys = div.querySelector('.keys');
-  console.log(keys);
-  // toggle active class on or off
+  // toggle active class on or off, add or remove eventListeners
   if (div.classList.contains('active')) {
     div.classList.remove('active');
     keys.classList.remove('active');
+    keys.removeEventListener('click', playSound);
+    div.removeEventListener('keypress', playSound);
   } else {
     div.classList.add('active');
     keys.classList.add('active');
+    keys.addEventListener('click', playSound);
+    div.addEventListener('keypress', playSound);
   }
 }
 
 // Event Listeners
 // Drums
-drumKeys.forEach(key => key.addEventListener('click', playSound));  // Listen for drumKeys to be pressed
 drummer.addEventListener('click', toggleActive);  // Listen for drummer to be clicked
 
 // Guitar
 
 // Bass
-
-// Listen for any key press
 window.addEventListener('keypress', playSound);
