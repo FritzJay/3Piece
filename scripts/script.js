cb7bd3e9d27dc3116df2754880b03b007e81e2cd
Jonathan F. Jay <Jonathan.jay10@gmail.com>
2017-04-28 12:59:59 -0700

    Set an early return in handleRecordClick if the user clicks while the button is primed.

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,7 @@ const recordButton = document.getElementById('record-btn');
 const playButton = document.getElementById('play-btn');
 const saveButtons = document.querySelectorAll('.save-btn');
 let isRecording = false;
+let isPrimed = false;                                 // Used to stop countdown early if user clicks record again
 let startRecording = Date.now();
 let drumsRecording = [];
 let guitarRecording = [];
@@ -238,6 +239,7 @@ function record (instrument, button, timestamp) {
 
 // Toggles isRecording and active class on button
 function handleRecordClick () {
+  if (recordButton.classList.contains('primed')) { return; }
   // Makes record button pulse
   function pulse () {
     recordButton.classList.add('pulse');
